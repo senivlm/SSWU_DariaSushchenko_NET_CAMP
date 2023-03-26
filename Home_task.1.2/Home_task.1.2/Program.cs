b761bdec39a61ca32837deee6612e9dbe9236b10
Daria Sushchenko <daria.gvendoline@gmail.com>
2023-03-26 19:20:29 +0300

    first change

---
+++ Home_task.1.2/Home_task.1.2/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace Home_task._1._2
+{
+    internal class Program
+    {
+        static void Print(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        static void FindLongestLine(int[,] matrix) 
+        {
+            int longestColor = 0;
+            int longestStartRow = 0;
+            int longestStartCol = 0;
+            int longestEndRow = 0;
+            int longestEndCol = 0;
+            int longestLineLength = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int currentColor = 0;
+                int currentStartRow = 0;
+                int currentStartCol = 0;
+                int currentLength = 0;
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    int color = matrix[i, j];
+                    if (color == currentColor)
+                    {
+                        currentLength++;
+                    }
+                    else
+                    {
+                        if (currentLength > longestLineLength)
+                        {
+                            longestColor = currentColor;
+                            longestStartRow = currentStartRow;
+                            longestStartCol = currentStartCol;
+                            longestEndRow = i;
+                            longestEndCol = j - 1;
+                            longestLineLength = currentLength;
+                        }
+                        currentColor = color;
+                        currentStartRow = i;
+                        currentStartCol = j;
+                        currentLength = 1;
+                    }
+                }
+
+                if (currentLength > longestLineLength)
+                {
+                    longestColor = currentColor;
+                    longestStartRow = currentStartRow;
+                    longestStartCol = currentStartCol;
+                    longestEndRow = i;
+                    longestEndCol = matrix.GetLength(1) - 1;
+                    longestLineLength = currentLength;
+                }
+            }
+            Console.WriteLine("Color: " + longestColor + " Start: " + longestStartRow + "," + longestStartCol + " End: " + longestEndRow + "," + longestEndCol + " Length: " + longestLineLength);
+        }
+
+        static void Main(string[] args)
+        {
+            int nRows = 4;
+            int nCols = 8;
+            int[,] matrix = new int[nRows, nCols];
+
+            Random rand = new Random();
+
+            for (int i = 0; i < nRows; i++)
+            {
+                for (int j = 0; j < nCols; j++)
+                {
+                    matrix[i, j] = rand.Next(0, 17);
+                }
+            }
+
+            Print(matrix);
+            FindLongestLine(matrix);
+
+        }
+    }
+}
---
+++ Home_task_1/Home_task_1/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace Home_task_1
+{
+    internal class Program
+    {
+        static void Print(int[,] matrix, int n, int m)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        static void FillMatrixСounterclock(int[,] matrix, int n, int m)
+        {
+            Console.WriteLine("Matrix Сounterclock-wise");
+            int i = 0, j = 0, p = 1;
+
+            while (i < n * m)
+            {
+                for (j = i; j <= n - 2 - i; j++)
+                {
+                    matrix[j, i] = p++;
+                }
+
+                for (j = i; j <= m - 2 - i; j++)
+                {
+                    matrix[n - i - 1, j] = p++;
+                }
+
+                for (j = n - 1 - i; j >= i + 1; j--)
+                {
+                    matrix[j, m - 1 - i] = p++;
+                }
+
+                for (j = m - 1 - i; j >= i + 1; j--)
+                {
+                    matrix[i, j] = p++;
+                }
+
+                i++;
+            }
+
+            if (i % 2 != 0)
+            {
+                matrix[(n - 1) / 2, (m - 1) / 2] = p;
+            }
+        }
+
+        static void FillMatrixClockwise(int[,] matrix, int n, int m)
+        {
+            Console.WriteLine("Matrix Clockwise");
+            int i = 0, j = 0, p = 1;
+
+            while (i < n * m)
+            {
+                for (j = i; j <= n - 2 - i; j++)
+                {
+                    matrix[i, j] = p++;
+                }
+
+                for (j = i; j <= m - 2 - i; j++)
+                {
+                    matrix[j, m - i - 1] = p++;
+                }
+
+                for (j = m - 1 - i; j >= i + 1; j--)
+                {
+                    matrix[n - 1 - i, j] = p++;
+                }
+
+                for (j = n - 1 - i; j >= i + 1; j--)
+                {
+                    matrix[j, i] = p++;
+                }
+
+                i++;
+            }
+
+            if (i % 2 != 0)
+            {
+                matrix[(n - 1) / 2, (m - 1) / 2] = p;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of rows and columns");
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, m];
+
+            FillMatrixСounterclock(matrix, n, m);
+            Console.WriteLine();
+            Print(matrix, n, m);
+
+            FillMatrixClockwise(matrix, n, m);
+            Console.WriteLine();
+            Print(matrix, n, m);
+        }
+    }
+}
